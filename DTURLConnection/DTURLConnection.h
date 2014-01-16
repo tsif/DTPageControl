68c2106ad6a70e8d57bd3f147292796eb1b3c3bb
tsif <tsiflitzis@gmail.com>
2014-01-16 10:33:58 +0200

    Created interface

--- DTURLConnection/DTURLConnection.h
+++ DTURLConnection/DTURLConnection.h
@@ -6,3 +6,20 @@
  * Use for concurrent multiple request within a single object
  *
  */
+
+#import <Foundation/Foundation.h>
+
+@interface DTURLConnection : NSURLConnection {
+    
+}
+
+/* add/replace info */
+- (void)setMetaObject:(id)object forKey:(NSString*)key;
+
+/* get info */
+- (id)metaObjectForKey:(NSString*)key;
+
+/* remove info */
+- (void)removeMetaObjectForKey:(NSString*)key;
+
+@end
