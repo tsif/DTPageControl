9ef1a9cb165f58015bb92d42b675dc265c0b387b
tsif <tsiflitzis@gmail.com>
2014-01-16 10:57:30 +0200

    initial implementation

--- DTURLConnection/DTURLConnection.m
+++ DTURLConnection/DTURLConnection.m
@@ -5,3 +5,62 @@
  * Useful for concurrent multiple request within a single object
  *
  */
+
+
+@interface DTURLConnection () {
+    NSMutableDictionary *_data;
+}
+
+@end
+
+@implementation DTURLConnection
+
+#pragma mark - LIFE CYCLE
+
+- (id)initWithRequest:(NSURLRequest *)request delegate:(id)delegate {
+    
+	self = [super initWithRequest:request delegate:delegate];
+	if(self) {
+		_data = [[NSMutableDictionary alloc] init];
+	}
+	return self;
+}
+
+#pragma mark - PROPERTIES
+
+- (void)setMetaObject:(id)object forKey:(NSString *)key {
+    
+    /* Add or replace a meta info property */
+	if((nil != object) && (nil != key) ){
+		[_data setObject:object forKey:key];
+	}	
+}
+
+- (id)metaObjectForKey:(NSString*)key {
+    
+	id result = nil;
+	if(key) {
+		result = [_data objectForKey:key];
+	}
+	return result;	
+}
+
+- (void)removeMetaObjectForKey:(NSString *)key {
+    
+	if(nil != key){
+		[_data removeObjectForKey:key];
+	}	
+}
+
+#pragma mark - CLEANUP CREW
+
+- (void)dealloc {
+    
+#if __has_feature(objc_arc)
+#else
+	[_data release];
+	[super dealloc];
+#endif
+}
+
+@end
